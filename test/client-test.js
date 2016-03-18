8a6ba4c50681df84ec3a52dab9920aa6a2573606
Sharmaz <irae45@gmail.com>
2016-03-18 02:50:06 -0600

    Agregamos el test e implementación de la función de search

--- test/client-test.js
+++ test/client-test.js
@@ -29,3 +29,21 @@ test('should list shows', function (t) {
     t.end()
   })
 })
+
+test('should search shows', function (t) {
+  var client = tvmaze.createClient({ endpoint: endpoint })
+
+  t.equals(typeof client.search, 'function', 'should be a function')
+
+  nock(endpoint)
+    .get('/search/shows')
+    .query({ q: 'daredevil' })
+    .reply(200, [{ name: 'Daredevil' }])
+
+  client.search('daredevil', function (err, shows) {
+    t.error(err, 'should not be an error')
+    t.ok(Array.isArray(shows), 'should be an array')
+    t.equals(shows[0].name, 'Daredevil', 'should retrieve a show name')
+    t.end()
+  })
+})
